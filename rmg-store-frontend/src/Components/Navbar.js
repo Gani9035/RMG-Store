d3e6984cb6b7c491bfb0e67508f341a357b56dfc
Gani9035 <ganeshrajmallayya777@gmail.com>
2024-02-22 23:00:12 +0530

    frontend

--- rmg-store-frontend/src/Components/Navbar.js
+++ rmg-store-frontend/src/Components/Navbar.js
@@ -157,7 +157,7 @@ const Navbar = ({children,setShow,setShow1,setImg}) => {
       <Menu />
      </IconButton>}
      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        RMG STORE
+        RMG STOREs
      </Typography>
      <Button color="inherit" onClick={HandleClick}>Sign Out</Button>
     </Toolbar>
